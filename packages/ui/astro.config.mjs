a6073cc57c64d492d00badfae1f18faf5735d370
Alexander Greene <greenealexander036@gmail.com>
2022-04-02 17:38:50 -0500

    create layout and swap in react for solid

--- packages/ui/astro.config.mjs
+++ packages/ui/astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
-import solid from "@astrojs/solid-js";
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solid(), tailwind()],
+  integrations: [react(), tailwind()],
 });
--- packages/ui/src/components/Button.jsx
+++ packages/ui/src/components/Button.jsx
@@ -1,28 +1,28 @@
-import { createSignal } from "solid-js";
+import React, { useState } from "react";
 
 export default function Button({ children }) {
-  const [count, setCount] = createSignal(0);
-  const add = () => setCount(count() + 1);
-  const subtract = () => setCount(count() - 1);
+  const [count, setCount] = useState(0);
+  const add = () => setCount(count + 1);
+  const subtract = () => setCount(count - 1);
 
   return (
     <>
-      <div class="counter">
+      <div>
         <button
           onClick={subtract}
-          class="py-2 px-4 bg-purple-500 text-white font-semibold rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75"
+          className="py-2 px-4 bg-purple-500 text-white font-semibold rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75"
         >
           -
         </button>
-        <pre>{count()}</pre>
+        <pre>{count}</pre>
         <button
           onClick={add}
-          class="py-2 px-4 bg-purple-500 text-white font-semibold rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75"
+          className="py-2 px-4 bg-purple-500 text-white font-semibold rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75"
         >
           +
         </button>
       </div>
-      <div class="counter-message">{children}</div>
+      <div>{children}</div>
     </>
   );
 }
