be200fe3884fcb8f55fa422e4c59a004d91f9d08
Bratislav Petkovic <brp221@lehigh.edu>
2022-04-14 15:06:19 -0400

    help me god

--- bokeh_dashboard/main.py
+++ bokeh_dashboard/main.py
@@ -20,7 +20,7 @@ from bokeh.layouts import column, row, gridplot
 from bokeh.models import ColumnDataSource, LabelSet, Div, TextInput, CustomJS,MultiChoice, RangeSlider, Button, FactorRange, DataTable, TableColumn, Column
 #from bokeh.transform import factor_cmap, factor_mark
 from bokeh.models.widgets import Tabs, Panel, MultiSelect, Paragraph, Select
-from bokeh.transform import factor_cmap
+from bokeh.transform import factor_cmap, dodge
 from bokeh.palettes import Spectral6, Set3, Plasma6
 
 import pandas as pd
@@ -34,7 +34,10 @@ from sqlalchemy import select
 from sqlalchemy import table
 from sqlalchemy import text
 
-from helperLibrary.library import *
+# from helperLibrary.library import *
+
+import plotly.graph_objects as go
+import plotly.offline as pyo
 
 
 import os
@@ -42,6 +45,29 @@ from dotenv import load_dotenv
 import fmpsdk
 load_dotenv()
 apikey=os.environ.get("apikey")
+
+
+
+# ___________________________________________________________________________________________________________________________________
+#                                  HELPER FUNCTIONS
+# For Python 3.0 and later
+from urllib.request import urlopen
+import certifi
+import json
+def get_jsonparsed_data(url):
+    response = urlopen(url, cafile=certifi.where())
+    data = response.read().decode("utf-8")
+    return json.loads(data)
+
+def column_formatter(df):
+    columns=[]
+    for i in df.columns:
+        columns.append(TableColumn(field=i, title=i))
+    return columns
+
+
+
+
 
 # ___________________________________________________________________________________________________________________________________
 #                                   CHARTS + VISUALIZATIONS
@@ -108,7 +134,7 @@ w = 12*60*60*1000 # half day in ms
 
 TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
 
-candlestick = figure(x_axis_type="datetime", tools=TOOLS, width=1000, title = randomStock + " Price")
+candlestick = figure(x_axis_type="datetime", tools=TOOLS, width=750,height=400, title = randomStock + " Price")
 candlestick.xaxis.major_label_orientation = pi/4
 candlestick.grid.grid_line_alpha=0.3
 #hovertool
@@ -125,7 +151,6 @@ estimatedEarningsDF = pd.DataFrame.from_dict(get_jsonparsed_data(url))
 incomeStatURL = ("https://financialmodelingprep.com/api/v3/income-statement/NVDA?limit=4&apikey=ce687b3fe0554890e65d6a5e48f601f9")
 incomeStatementDF  = pd.DataFrame.from_dict(get_jsonparsed_data(incomeStatURL))
 
-
 metrics = ['Revenue', 'Earnings', "EBITDA"]
 years = ['2019', '2020', '2021', '2022']
 
@@ -135,27 +160,137 @@ data = {'metrics' : metrics,
         '2021'   : [incomeStatementDF.revenue[1],incomeStatementDF.netIncome[1], incomeStatementDF.ebitda[1]],
         '2022'   : [incomeStatementDF.revenue[0],incomeStatementDF.netIncome[0], incomeStatementDF.ebitda[0]]}
 
-
 x = [ (metric, year) for metric in metrics for year in years ]
 counts = sum(zip(data['2019'], data['2020'], data['2021'], data['2022']), ()) # like an hstack
 
 source = ColumnDataSource(data=dict(x=x, counts=counts))
-
 earningsBar = figure(x_range=FactorRange(*x), height=250, title="Fruit counts by year",
            toolbar_location=None, tools="")
-
 earningsBar.vbar(x='x', top='counts', width=0.9, source=source, line_color="white",
        fill_color=factor_cmap('x', palette=Plasma6, factors=years, start=1, end=2))
-
 earningsBar.y_range.start = 0
 earningsBar.x_range.range_padding = 0.1
 earningsBar.xaxis.major_label_orientation = 1
 earningsBar.xgrid.grid_line_color = None
 
+#       <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<   LINEGRAPH EARNINGS  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+url = ("https://financialmodelingprep.com/api/v3/analyst-estimates/NVDA?limit=4&apikey=ce687b3fe0554890e65d6a5e48f601f9")
+estimatedEarningsDF = pd.DataFrame.from_dict(get_jsonparsed_data(url))
+
+# QUARTER : api/v3/income-statement/AAPL?period=quarter&limit=400&apikey=ce687b3fe0554890e65d6a5e48f601f9
+incomeStatURL = ("https://financialmodelingprep.com/api/v3/income-statement/NVDA?limit=4&apikey=ce687b3fe0554890e65d6a5e48f601f9")
+incomeStatementDF  = pd.DataFrame.from_dict(get_jsonparsed_data(incomeStatURL))
+
+incomeStatURLquarter = ("https://financialmodelingprep.com/api/v3/income-statement/NVDA?period=quarter&limit=16&apikey=ce687b3fe0554890e65d6a5e48f601f9")
+incomeStatDFQuart = pd.DataFrame.from_dict(get_jsonparsed_data(incomeStatURLquarter))
+
+incomeStatementDF.index = pd.to_datetime(incomeStatementDF.date)
+incomeStatDFQuart.index = pd.to_datetime(incomeStatDFQuart.date)
+# incomeStatementDF.year = ["2022","2021", "2020", "2019"]
+
+TOOLS = 'save,pan,box_zoom,reset,wheel_zoom, hover'
+lineGraph = figure(title="Year-wise total number of crimes",x_axis_type="datetime",  y_axis_type="linear", plot_height = 400,
+           tools = TOOLS, plot_width = 800)
+lineGraph.xaxis.axis_label = 'Year'
+lineGraph.yaxis.axis_label = 'Metrics'
+lineGraph.line(incomeStatDFQuart.index ,incomeStatDFQuart.revenue , line_color="#0C0786", line_width = 3)
+lineGraph.line(incomeStatDFQuart.index ,incomeStatDFQuart.netIncome , line_color="#B02A8F", line_width = 3)
+lineGraph.line(incomeStatDFQuart.index ,incomeStatDFQuart.ebitda , line_color="#FCA635", line_width = 3)
+
+show(lineGraph)
+# # p.line('date', 'netIncome', source=source)
+# p.add_tools(HoverTool(('year', '@x'),
+# ('revenue', '@y'),
+# ('netIncome', '@y')))
+
+# show the tooltip
+# p.add_tools(HoverTool(
+#     tooltips='<font face="Arial" size="3">WYear: @x{0.0}, Revenue/Income is @y{0.00}</font>',
+#     mode='vline'
+# ))
+
 
 #       <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<   INSIDER TRADING CHART  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-#       <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<    SPYDER CHART (PEERS)  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#       <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<    COMPANY HEALTH vs (PEERS)  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+outlookURL = ("https://financialmodelingprep.com/api/v4/company-outlook?symbol=AAPL&apikey=ce687b3fe0554890e65d6a5e48f601f9")
+aaplOutlook  =  pd.DataFrame.from_dict(get_jsonparsed_data(outlookURL)["rating"])
+
+outlookURL2 = ("https://financialmodelingprep.com/api/v4/company-outlook?symbol=MSFT&apikey=ce687b3fe0554890e65d6a5e48f601f9")
+peerOutlook  =  pd.DataFrame.from_dict(get_jsonparsed_data(outlookURL2)["rating"])
+
+outlookURL3 = ("https://financialmodelingprep.com/api/v4/company-outlook?symbol=GOOGL&apikey=ce687b3fe0554890e65d6a5e48f601f9")
+peerOutlook3  =  pd.DataFrame.from_dict(get_jsonparsed_data(outlookURL3)["rating"])
+categories = ["DCF", "ROE", "ROA", "D/E", "P/E", "P/B"]
+categories=[*categories, categories[0]]
+x =  [aaplOutlook.ratingDetailsDCFScore[0], aaplOutlook.ratingDetailsROEScore[0], aaplOutlook.ratingDetailsROAScore[0], 
+               aaplOutlook.ratingDetailsDEScore[0], aaplOutlook.ratingDetailsPEScore[0], aaplOutlook.ratingDetailsPBScore[0]]
+x2 =  [peerOutlook.ratingDetailsDCFScore[0], peerOutlook.ratingDetailsROEScore[0], peerOutlook.ratingDetailsROAScore[0], 
+               peerOutlook.ratingDetailsDEScore[0], peerOutlook.ratingDetailsPEScore[0], peerOutlook.ratingDetailsPBScore[0]]
+x3 =  [peerOutlook3.ratingDetailsDCFScore[0], peerOutlook3.ratingDetailsROEScore[0], peerOutlook3.ratingDetailsROAScore[0], 
+               peerOutlook3.ratingDetailsDEScore[0], peerOutlook3.ratingDetailsPEScore[0], peerOutlook3.ratingDetailsPBScore[0]]
+x=[*x, x[0]]
+x2=[*x2, x2[0]]
+x3=[*x3, x3[0]]
+
+fig = go.Figure(
+    data=[
+        go.Scatterpolar(r=x, theta=categories, fill='toself', name='AAPL'),
+        go.Scatterpolar(r=x2, theta=categories, fill='toself', name='MSFT'),
+        go.Scatterpolar(r=x3, theta=categories, fill='toself', name='GOOGL')
+    ],
+    layout=go.Layout(
+        #title=go.layout.Title(text='COMPANY HEALTH COMPARISON'),
+        #polar={'radialaxis': {'visible': True}},
+        showlegend=True,
+        width=400,
+        height=400
+    )
+)
+output_file("radarChart.html")
+fig.write_image("radarChart.jpg")
+#bokeh_dashboard/static_content/
+radarChartStatic = Div(text="""<img src="bokeh_dashboard/static_content/radarChart.png" alt="radarChartStatic"> """,width=200, height=100)
+show(radarChartStatic)
+div_image = Div(text="""<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" alt="div_image">""", width=150, height=150)
+show(div_image)
+#       <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<    COMPANY HEALTH vs (PEERS)  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+factors = ["DCF", "ROE", "ROI", "D/E", "P/E", "P/B"]
+years = ['AAPL', 'PEER1', 'PEER2']
+
+x =  [aaplOutlook.ratingDetailsDCFScore[0], aaplOutlook.ratingDetailsROEScore[0], aaplOutlook.ratingDetailsROAScore[0], 
+               aaplOutlook.ratingDetailsDEScore[0], aaplOutlook.ratingDetailsPEScore[0], aaplOutlook.ratingDetailsPBScore[0]]
+x2 =  [peerOutlook.ratingDetailsDCFScore[0], peerOutlook.ratingDetailsROEScore[0], peerOutlook.ratingDetailsROAScore[0], 
+               peerOutlook.ratingDetailsDEScore[0], peerOutlook.ratingDetailsPEScore[0], peerOutlook.ratingDetailsPBScore[0]]
+x3 =  [peerOutlook3.ratingDetailsDCFScore[0], peerOutlook3.ratingDetailsROEScore[0], peerOutlook3.ratingDetailsROAScore[0], 
+               peerOutlook3.ratingDetailsDEScore[0], peerOutlook3.ratingDetailsPEScore[0], peerOutlook3.ratingDetailsPBScore[0]]
+
+data = {'factors' : factors,
+        'AAPL'    : x,
+        'PEER1'   : x2,
+        'PEER2'   : x3}
+
+source = ColumnDataSource(data=data)
+
+compHealthHBar = figure(y_range=factors, x_range=(0, 7), plot_width=400,plot_height=500, title="COMPANY HEALTH vs (PEERS)",
+           toolbar_location=None, tools="")
+
+compHealthHBar.hbar(y=dodge('factors', -0.25, range=compHealthHBar.y_range), right='AAPL', height=0.2, source=source,
+       color="#c9d9d3", legend_label="AAPL")
+
+compHealthHBar.hbar(y=dodge('factors',  0.0,  range=compHealthHBar.y_range), right='PEER1', height=0.2, source=source,
+       color="#718dbf", legend_label="PEER1")
+
+compHealthHBar.hbar(y=dodge('factors',  0.25, range=compHealthHBar.y_range), right='PEER2', height=0.2, source=source,
+       color="#e84d60", legend_label="PEER2")
+
+compHealthHBar.y_range.range_padding = 0.1
+compHealthHBar.ygrid.grid_line_color = None
+compHealthHBar.legend.location = "top_right"
+compHealthHBar.legend.orientation = "vertical"
 
 
 # ___________________________________________________________________________________________________________________________________
@@ -193,13 +328,18 @@ marketCapSlider.js_on_change("value", CustomJS(code="""
 # ___________________________________________________________________________________________________________________________________
 #                                   LAYOUT
 
-# analystRatingColumn = column(analystRatingDT)
+annualLayout = layout([[earningsBar]], sizing_mode='fixed')
+quarterLayout = layout([[lineGraph]],sizing_mode='fixed')
+annualTab = Panel(child=annualLayout,title="Annual")
+quartersTab = Panel(child=quarterLayout,title="Quarter")
+
+earningsTab = Tabs(tabs=[annualTab, quartersTab])
 
 grid = gridplot([ [analystRatingDT,bestValueDT ],
                   [biggestGrowersDT,healthiestCompsDT ]  ])
-earningsBar
 
-gridTab2 = gridplot([ [candlestick,earningsBar ] ])
+
+gridTab2 = gridplot([ [candlestick,earningsTab ], [compHealthHBar, radarChartStatic, div_image] ])
 
 columnWidgets = Column(sectorChoice, join1, join2, marketCapSlider)
                        
--- bokeh_dashboard/testCode.py
+++ bokeh_dashboard/testCode.py
@@ -1,14 +1,15 @@
 from helperLibrary import *
-
+from .library import *
 # /api/v3/analyst-estimates/AAPL?limit=30
 
 from bokeh.io import output_file, show
 from bokeh.models import ColumnDataSource
-from bokeh.plotting import figure
+from bokeh.plotting import figure, show, output_notebook
+
 from bokeh.transform import dodge, factor_cmap
 from bokeh.palettes import Spectral6, Set3, Plasma6
 
-
+output_notebook()
 #       <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<   VBAR  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 url = ("https://financialmodelingprep.com/api/v3/analyst-estimates/NVDA?limit=4&apikey=ce687b3fe0554890e65d6a5e48f601f9")
 estimatedEarningsDF = pd.DataFrame.from_dict(get_jsonparsed_data(url))
@@ -16,6 +17,9 @@ estimatedEarningsDF = pd.DataFrame.from_dict(get_jsonparsed_data(url))
 # QUARTER : api/v3/income-statement/AAPL?period=quarter&limit=400&apikey=ce687b3fe0554890e65d6a5e48f601f9
 incomeStatURL = ("https://financialmodelingprep.com/api/v3/income-statement/NVDA?limit=4&apikey=ce687b3fe0554890e65d6a5e48f601f9")
 incomeStatementDF  = pd.DataFrame.from_dict(get_jsonparsed_data(incomeStatURL))
+
+incomeStatURLquarter = ("https://financialmodelingprep.com/api/v3/income-statement/NVDA?period=quarter&limit=16&apikey=ce687b3fe0554890e65d6a5e48f601f9")
+incomeStatDFQuart = pd.DataFrame.from_dict(get_jsonparsed_data(incomeStatURLquarter))
 
 
 metrics = ['Revenue', 'Earnings', "EBITDA"]
@@ -32,7 +36,7 @@ x = [ (metric, year) for metric in metrics for year in years ]
 counts = sum(zip(data['2019'], data['2020'], data['2021'], data['2022']), ()) # like an hstack
 
 source = ColumnDataSource(data=dict(x=x, counts=counts))
-
+output_file("radar.html")
 p = figure(x_range=FactorRange(*x), height=250, title="Fruit counts by year",
            toolbar_location=None, tools="")
 
@@ -44,4 +48,208 @@ p.x_range.range_padding = 0.1
 p.xaxis.major_label_orientation = 1
 p.xgrid.grid_line_color = None
 
+show(p)
+
+from bokeh.plotting import figure, show
+
+num_vars = 6
+outlookURL = ("https://financialmodelingprep.com/api/v4/company-outlook?symbol=AAPL&apikey=ce687b3fe0554890e65d6a5e48f601f9")
+aaplOutlook  =  pd.DataFrame.from_dict(get_jsonparsed_data(outlookURL)["rating"])
+
+outlookURL2 = ("https://financialmodelingprep.com/api/v4/company-outlook?symbol=MSFT&apikey=ce687b3fe0554890e65d6a5e48f601f9")
+peerOutlook  =  pd.DataFrame.from_dict(get_jsonparsed_data(outlookURL2)["rating"])
+
+
+
+import numpy as np
+import pandas as pd
+import holoviews as hv
+from holoviews import dim, opts
+
+from bokeh.models import Div
+
+hv.extension('bokeh')
+img = hv.Image(np.random.rand(10, 10))
+
+fig = hv.render(img)
+
+print('Figure: ', fig)
+print('Renderers: ', fig.renderers[-1].glyph)
+
+
+violin = hv.Violin(np.random.randn(100))
+
+hv.output(violin, fig='png')
+hv.save(violin, 'violin.png')
+
+
+html = file_html(violin, CDN, "my plot")
+
+
+div = Div(text="""Your <img src="violin.png" alt="Italian Trulli"> """,
+width=200, height=100)
+
+show(div)
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+categories = ['Food Quality', 'Food Variety', 'Service Quality', 'Ambiance', 'Affordability']
+
+restaurant_1 = [4, 4, 5, 4, 3]
+restaurant_2 = [5, 5, 4, 5, 2]
+restaurant_3 = [3, 4, 5, 3, 5]
+
+label_loc = np.linspace(start=0, stop=2 * np.pi, num=len(restaurant_1))
+
+plt.figure(figsize=(8, 8))
+plt.subplot(polar=True)
+plt.plot(label_loc, restaurant_1, label='Restaurant 1')
+plt.plot(label_loc, restaurant_2, label='Restaurant 2')
+plt.plot(label_loc, restaurant_3, label='Restaurant 3')
+plt.title('Restaurant comparison', size=20)
+lines, labels = plt.thetagrids(np.degrees(label_loc), labels=categories)
+plt.legend()
+plt.show()
+
+
+
+
+import plotly.graph_objects as go
+import plotly.offline as pyo
+
+categories = ["DCF", "ROE", "ROA", "D/E", "P/E", "P/B"]
+categories=[*categories, factors[0]]
+x =  [aaplOutlook.ratingDetailsDCFScore[0], aaplOutlook.ratingDetailsROEScore[0], aaplOutlook.ratingDetailsROAScore[0], 
+               aaplOutlook.ratingDetailsDEScore[0], aaplOutlook.ratingDetailsPEScore[0], aaplOutlook.ratingDetailsPBScore[0]]
+x2 =  [peerOutlook.ratingDetailsDCFScore[0], peerOutlook.ratingDetailsROEScore[0], peerOutlook.ratingDetailsROAScore[0], 
+               peerOutlook.ratingDetailsDEScore[0], peerOutlook.ratingDetailsPEScore[0], peerOutlook.ratingDetailsPBScore[0]]
+x3 =  [peerOutlook3.ratingDetailsDCFScore[0], peerOutlook3.ratingDetailsROEScore[0], peerOutlook3.ratingDetailsROAScore[0], 
+               peerOutlook3.ratingDetailsDEScore[0], peerOutlook3.ratingDetailsPEScore[0], peerOutlook3.ratingDetailsPBScore[0]]
+x=[*x, x[0]]
+x2=[*x2, x2[0]]
+x3=[*x3, x3[0]]
+
+
+
+categories = ['Food Quality', 'Food Variety', 'Service Quality', 'Ambience', 'Affordability']
+categories = [*categories, categories[0]]
+
+restaurant_1 = [4, 4, 5, 4, 3]
+restaurant_2 = [5, 5, 4, 5, 2]
+restaurant_3 = [3, 4, 5, 3, 5]
+restaurant_1 = [*restaurant_1, restaurant_1[0]]
+restaurant_2 = [*restaurant_2, restaurant_2[0]]
+restaurant_3 = [*restaurant_3, restaurant_3[0]]
+
+
+fig = go.Figure(
+    data=[
+        go.Scatterpolar(r=x, theta=categories, fill='toself', name='AAPL'),
+        go.Scatterpolar(r=x2, theta=categories, fill='toself', name='MSFT'),
+        go.Scatterpolar(r=x3, theta=categories, fill='toself', name='GOOGL')
+    ],
+    layout=go.Layout(
+        title=go.layout.Title(text='COMPANY HEALTH COMPARISON'),
+        polar={'radialaxis': {'visible': True}},
+        showlegend=True,
+        width=500,
+        height=500
+    )
+)
+
+pyo.plot(fig)
+
+fig.write_image("images/radarChart.png")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import numpy as np
+from bokeh.plotting import figure, show, output_file
+from bokeh.models import ColumnDataSource, LabelSet
+
+num_vars = 6
+outlookURL = ("https://financialmodelingprep.com/api/v4/company-outlook?symbol=AAPL&apikey=ce687b3fe0554890e65d6a5e48f601f9")
+aaplOutlook  =  pd.DataFrame.from_dict(get_jsonparsed_data(outlookURL)["rating"])
+
+outlookURL2 = ("https://financialmodelingprep.com/api/v4/company-outlook?symbol=MSFT&apikey=ce687b3fe0554890e65d6a5e48f601f9")
+peerOutlook  =  pd.DataFrame.from_dict(get_jsonparsed_data(outlookURL2)["rating"])
+
+
+centre = 0.5
+
+theta = np.linspace(0, 2*np.pi, num_vars, endpoint=False)
+# rotate theta such that the first axis is at the top
+theta += np.pi/2
+print(theta)
+def unit_poly_verts(theta, centre ):
+    """Return vertices of polygon for subplot axes.
+    This polygon is circumscribed by a unit circle centered at (0.5, 0.5)
+    """
+    x0, y0, r = [centre ] * 3
+    print(x0)
+    print(y0)
+    print(r)
+    verts = [(r*np.cos(t) + x0, r*np.sin(t) + y0) for t in theta]
+    return verts
+
+def radar_patch(r, theta, centre ):
+    """ Returns the x and y coordinates corresponding to the magnitudes of 
+    each variable displayed in the radar plot
+    """
+    # offset from centre of circle
+    offset = 0.01
+    yt = (r*centre + offset) * np.sin(theta) + centre 
+    xt = (r*centre + offset) * np.cos(theta) + centre 
+    return xt, yt
+
+verts = unit_poly_verts(theta, centre)
+x = [v[0] for v in verts] 
+y = [v[1] for v in verts] 
+
+p = figure(title="Baseline - Radar plot")
+factors = ["DCF", "ROE", "ROI", "D/E", "P/E", "P/B"]
+years = ['AAPL', 'PEER1', 'PEER2']
+source = ColumnDataSource({'x':x + [centre ],'y':y + [1],'factors':factors})
+
+p.line(x="x", y="y", source=source)
+x =  [aaplOutlook.ratingDetailsDCFScore[0], aaplOutlook.ratingDetailsROEScore[0], aaplOutlook.ratingDetailsROAScore[0], 
+               aaplOutlook.ratingDetailsDEScore[0], aaplOutlook.ratingDetailsPEScore[0], aaplOutlook.ratingDetailsPBScore[0]]
+x2 =  [peerOutlook.ratingDetailsDCFScore[0], peerOutlook.ratingDetailsROEScore[0], peerOutlook.ratingDetailsROAScore[0], 
+               peerOutlook.ratingDetailsDEScore[0], peerOutlook.ratingDetailsPEScore[0], peerOutlook.ratingDetailsPBScore[0]]
+x3 =  [peerOutlook3.ratingDetailsDCFScore[0], peerOutlook3.ratingDetailsROEScore[0], peerOutlook3.ratingDetailsROAScore[0], 
+               peerOutlook3.ratingDetailsDEScore[0], peerOutlook3.ratingDetailsPEScore[0], peerOutlook3.ratingDetailsPBScore[0]]
+
+labels = LabelSet(x="x",y="y",text="factors",source=source)
+
+p.add_layout(labels)
+
+# example factor:
+f1 = np.array(x)
+f2 = np.array(x2)
+f3 = np.array(x3)
+
+#xt = np.array(x)
+flist = [f1,f2,f3]
+colors = ['blue','green','red', 'orange','purple']
+for i in range(len(flist)):
+    xt, yt = radar_patch(flist[i], theta, centre)
+    p.patch(x=xt, y=yt, fill_alpha=0.15, fill_color=colors[i])
 show(p)
