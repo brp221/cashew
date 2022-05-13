541466d1f9f1aa8096f717c658e4ee2ab7c928fa
Bratislav Petkovic <brp221@lehigh.edu>
2022-05-12 23:24:16 -0400

    BB-RSI-OBV is well under way

--- WaveSurfer/helper_functions.py
+++ WaveSurfer/helper_functions.py
@@ -16,17 +16,60 @@ def get_jsonparsed_data(url):
     data = response.read().decode("utf-8")
     return json.loads(data)
 
+def calc_slope(x):
+    slope = np.polyfit(range(len(x)), x, 1)[0]
+    return slope
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------
+import matplotlib.pyplot as plt
+
+def bb_graph(df, condition):
+    plt.rcParams["figure.figsize"] = [24,16]
+    plt.plot(df['date'],df['upper'], label="Up band")
+    plt.plot(df['date'],df['mid'], label='Mid band')
+    plt.plot(df['date'],df['lower'], label='Low band')
+    plt.plot(df['date'],df['close'],color = 'black')
+    
+    for i in range(0,len(condition)): 
+        plt.axvline(condition.iloc[i])
+    plt.show()
+
+def rsi_graph(df, condition):
+    plt.rcParams["figure.figsize"] = [24,16]
+    plt.plot(df['date'],df['RSI'], color='blue')
+    for i in range(0,len(condition)): 
+        plt.axvline(condition.iloc[i])
+    plt.show()
+
+def obv_graph(df, condition):
+    slope = df[df["OBV_slope"].notnull()]
+    plt.rcParams["figure.figsize"] = [24,16]
+    plt.plot(df['date'],df['OBV'], color='purple')
+    plt.plot(slope['date'],slope['OBV_slope'], color='green')
+    plt.show()
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
-
-
-
-#-------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-
+def big_picture(df, condition1, condition2, condition3):
+    plt.rcParams["figure.figsize"] = [32,24]
+    fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
+    ax1.plot(df['date'],df['upper'], label="Up band", color='green')
+    ax1.plot(df['date'],df['mid'], label='Mid band', color='orange')
+    ax1.plot(df['date'],df['lower'], label='Low band', color='red')
+    ax1.plot(df['date'],df['close'],color = 'blue')
+    for i in range(0,len(condition1)): 
+        ax1.axvline(condition1.iloc[i], color="green")
+    for i in range(0,len(condition3)): 
+        ax1.axvline(condition3.iloc[i], color="red")
+    
+    ax2.plot(df['date'],df['RSI'], color='blue')
+    for i in range(0,len(condition2)): 
+        ax2.axvline(condition2.iloc[i])
+    ax2.axhline(y = 70, color = 'r', linestyle = '-')
+    ax2.axhline(y = 30, color = 'r', linestyle = '-')
+    
+    ax3.plot(df['date'],df['OBV'], color='purple')
+    ax1.set_title('BB, RSI, OBV Strategy')
+    plt.show()
 
 
 
--- WaveSurfer/rsiBB.py
+++ WaveSurfer/rsiBB.py
@@ -22,37 +22,52 @@ symbol="AMZN"
 priceURL = ("https://financialmodelingprep.com/api/v3/historical-chart/"+time+"/"+symbol+"?apikey=ce687b3fe0554890e65d6a5e48f601f9")
 url = ("https://financialmodelingprep.com/api/v3/historical-price-full/AAPL?apikey=ce687b3fe0554890e65d6a5e48f601f9")
 msft = pd.DataFrame.from_dict(get_jsonparsed_data(priceURL))
-
-
-msft['RSI'] = talib.RSI(msft['close'], timeperiod=14)
-fig, (ax1, ax2) = plt.subplots(2, sharex=True)
-ax1.set_ylabel('Price')
-ax1.plot(msft['close'],color = 'black')
-ax2.set_ylabel('RSI')
-ax2.plot(msft['RSI'], color='blue')
-ax2.axhline(y = 70, color = 'r', linestyle = '-')
-ax2.axhline(y = 30, color = 'r', linestyle = '-')
-ax1.set_title('Daily Close Price and RSI')
-plt.show()
+msftR = msft[::-1]
 #------------------------------------------------------------------------------------------------------------------------------------------
-upper, mid, lower = talib.BBANDS(msft['close'], nbdevup=2, nbdevdn=2, timeperiod=20)
-plt.plot(upper, label="Upper band")
-plt.plot(mid, label='Middle band')
-plt.plot(lower, label='Lower band')
-plt.plot(msft['close'], label="Close")
-plt.title('Bollinger Bands')
-plt.legend()
-plt.show()
+msftR['RSI'] = talib.RSI(msftR['close'], timeperiod=14)
+msftR['upper'], msftR['mid'], msftR['lower'] = talib.BBANDS(msftR['close'], nbdevup=2, nbdevdn=2, timeperiod=20)
+msftR['OBV'] = talib.OBV(msftR['close'], msftR['volume'])
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
-fig, (ax1, ax2) = plt.subplots(2, sharex=True)
+plt.figure(figsize=(10, 10))
+fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
 ax1.set_ylabel('$')
-ax1.plot(upper, label="Upper band")
-ax1.plot(mid, label='Middle band')
-ax1.plot(lower, label='Lower band')
-ax1.plot(msft['close'],color = 'black')
+ax1.plot(msftR['date'],msftR['upper'], label="Up band")
+ax1.plot(msftR['date'],msftR['mid'], label='Mid band')
+ax1.plot(msftR['date'],msftR['lower'], label='Low band')
+ax1.plot(msftR['date'],msftR['close'],color = 'black')
 ax2.set_ylabel('RSI')
-ax2.plot(msft['RSI'], color='blue')
+ax2.plot(msftR['date'],msftR['RSI'], color='blue')
 ax2.axhline(y = 70, color = 'r', linestyle = '-')
 ax2.axhline(y = 30, color = 'r', linestyle = '-')
+ax3.plot(msftR['date'],msftR['OBV'], color='purple')
 ax1.set_title('BB and RSI')
 plt.show()
+#-------------------------------------------------------------------------------------------------------------------------------------------------------
+# inspiration: https://tradingstrategyguides.com/best-combination-of-technical-indicators/
+
+# Step #1: Price needs to Break and Close above the middle Bollinger Band
+condition_1 = msftR[msftR["mid"]<=msftR["close"]]
+bb_graph(msftR, condition_1["date"])
+
+# Step #2: Wait for the RSI indicator to trade above the 50 level if it doesn’t already
+condition_2 = msftR[msftR["RSI"]>50]
+rsi_graph(msftR, condition_2["date"])
+
+# Step #3: Wait for the OBV indicator to rise. Buy at the market once you see volume confirming the price.
+# result = msftR["OBV"].rolling(10, min_periods=2).apply(calc_slope)[4::5]
+# result.name="OBV_slope"
+# obv_data = msftR[['date','OBV']]
+# obv_data = pd.concat([obv_data, result],axis=1)
+# obv_graph(obv_data, "bla")
+#HOW TO DECIDE IF OBV IS INCREASING OR DECREASING 
+
+# Step #4: Hide your Protective Stop Loss below the lower Bollinger Band
+stopLossCondition = msftR[msftR["lower"]>=msftR["close"]]
+
+# Step #5: Take Profit when the price breaks below the lower BB
+cashInCondition = msftR[msftR["lower"]>msftR["close"]]
+bb_graph(msftR, cashInCondition["date"])
+
+#SEEMS AS THOUGH THE EXIT CONDITION IS ALWAYS THE SAME :):
+big_picture(msftR,condition_1["date"], condition_2["date"], stopLossCondition["date"])
+#-------------------------------------------------------------------------------------------------------------------------------------------------------
